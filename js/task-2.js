9b86cc15e244631c0f9110c219127d66b18ee886
myroslava <murosja80@gmail.com>
2024-12-07 14:48:26 +0200

    hw_02

--- js/task-2.js
+++ js/task-2.js
@@ -6,10 +6,11 @@ function getShippingMessage(country, price, deliveryFee) {
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
 console.log(getShippingMessage("Sweden", 100, 20));
+
 //---
 
 function formatMessage(message, maxLength) {
-  if (message <= maxLength) {
+  if (message.length <= maxLength) {
     return message;
   } else {
     return message.slice(0, maxLength) + "...";
@@ -22,3 +23,5 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+//---
